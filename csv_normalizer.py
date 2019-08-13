f43e9a6081fd21b3b769644595d55f3103cb18be
marciorpcoelho <marciorpcoelho@gmail.com>
2019-08-13 10:59:41 +0100

    get_all_csv_files will now return the names of the files without the csv termination; Added initial log_creation function which saves to a txt file the number of fixed lines and the file processing time; Removed method number 2; Moved a few controlling variables inside normalizer function so the codes is cleaner;

--- csv_normalizer.py
+++ csv_normalizer.py
@@ -1,6 +1,8 @@
 import sys
+from os import listdir
 import re
 import time
+
 
 regex_dict = {
     'new_lines': r'(\r\n|\r|\n)',
@@ -13,25 +15,31 @@ regex_dict = {
 
 
 def main(source, target):
-    # file_name = 'dbs/BIFD_SOHeaders_Comments_Orig'
-    # file_name_2 = 'dbs/BIFD_SOActivities_Orig'
-    # file_name_3 = 'D:\\BIFD_NLOpenAccountsMov'
-
-    files_name, in_files_name_and_path = get_all_csv_files(source)
-    out_files_name_and_path = [target + x for x in files_name]
-
     start = time.time()
 
-    for file_name, in_file_name_path, out_files_name_and_path in zip(files_name, in_files_name_and_path, out_files_name_and_path):
-        print('Normalizing file {}...'.format(file_name))
-        start_1 = time.time()
-        normalizer(file_name, in_file_name_path, out_files_name_and_path)
-        print('File {} normalized. Elapsed Time: {:.2f}'.format(file_name, (time.time() - start_1)))
+    files_name, in_files_name_and_path, out_files_name_and_path = get_all_csv_files(source, target)
+
+    for file_name, in_file_name_path, out_file_name_and_path in zip(files_name, in_files_name_and_path, out_files_name_and_path):
+        step_1_count, step_2_lines, normalize_time = normalizer(file_name, in_file_name_path, out_file_name_and_path)
+        log_creation(step_1_count, step_2_lines, file_name, target, normalize_time)
 
     print('Total Elapsed Time: {:.2f}'.format(time.time() - start))
 
 
-def multireplace(string, replacements):
+def log_creation(step_1_count, step_2_lines, file_name, target, timer):
+
+    log_file_name = file_name + '_log'
+    time_tag = time.strftime("%Y-%m-%d_%H.%M.%S")
+
+    with open(target + time_tag + '_' + log_file_name + '.txt', 'w') as log_file:
+        log_file.write('Log record for file {}:\n'.format(file_name))
+        log_file.write('Step 1 - Number of lines with wrongfully placed CRLF: {}\n'.format(step_1_count))
+        log_file.write('Step 2 - Number of line delimiters " corrected: {}\n'.format(int(len(step_2_lines) / 2)))
+        log_file.write('Normalizing time: {:.3f}\n'.format(timer))
+    log_file.close()
+
+
+def multireplace(string, replacements, step_2_lines):
     number_of_corrections = 0
 
     substrs = sorted(replacements, key=len, reverse=True)
@@ -46,30 +54,36 @@ def multireplace(string, replacements):
         if string[x.end() + number_of_corrections] == ';':  # Campos vazios
             continue
         else:
+            step_2_lines.append(string[1:-1])
             new_group = re.sub('"', '""', x.group(), count=1)  # Replaces " by "", effectively adding one ", only once
             string = ''.join((string[:x.start() + number_of_corrections], new_group, string[x.end() + number_of_corrections:]))
+            step_2_lines.append(string[1:-1])
             number_of_corrections += 1
 
-    return string
+    return string, step_2_lines
 
 
-def get_all_csv_files(source):
+def get_all_csv_files(source, target):
 
-    # onlyfiles = [f for f in listdir(source) if f.endswith('.csv')]
-    onlyfiles = ['BIFD_NLOpenAccountsMov.csv']
-    # onlyfiles = ['test.csv']  # For testing Purposes
+    # onlyfiles = [f[:-4] for f in listdir(source) if f.endswith('.csv')]
+    onlyfiles = ['BIFD_SOHeaders_Comments']
+    # onlyfiles = ['test.csv', 'test_2.csv']  # For testing Purposes
 
     if onlyfiles:
-        files_to_treat = [source + x for x in onlyfiles]
-        return onlyfiles, files_to_treat
+        source_files = [source + x + '.csv' for x in onlyfiles]
+        target_files = [target + x + '.csv' for x in onlyfiles]
+        return onlyfiles, source_files, target_files
     else:
         raise FileNotFoundError('Pasta de origem {} sem ficheiros csv.'.format(source))
 
 
 def normalizer(file_name, in_file_name, out_file_name):
+    print('Normalizing file {}...'.format(file_name))
+
+    start = time.time()
     reg = re.compile(regex_dict['new_lines'])
     reg_delimiter = re.compile(regex_dict['delimiter'])
-    count = 0
+    run_once, step_1_count, step_2_lines = 0, 0, []
 
     replacements = {
         regex_dict['plicas_middle']: 'middle',
@@ -77,112 +91,70 @@ def normalizer(file_name, in_file_name, out_file_name):
         regex_dict['plicas_right']: 'right',
     }
 
-    lines_step_1 = []
+    current_delimiter_count, delimiter_count = 0, 0
+    prev_line, total_line, line = '', '', ''
+    with open(in_file_name) as in_file:
+        with open(out_file_name, 'w') as out_file:
 
-    method = 3
+            for line in in_file:
+                # print('prev_line: {} \nline: {}'.format(prev_line, line))
+                if not run_once:
+                    delimiter_count = len(re.findall(reg_delimiter, line))
+                    # print('delimiter_count', delimiter_count)
+                run_once = 1
 
-    # Method #1
-    # for line in fileinput.input(in_file_name, inplace=1):
-    #
-    #     if in_file_name == 'C:\\Users\\mrpc\\Documents\\mrpc - Pessoal\\PyCharm Projects\\test_files\\BIFD_SOHeaders_Comments_Orig.csv' and '"208";"72";"2018/OT3/922";"20180313";"3727";"21/03 (JR) HABLO CON CLIENTA' in line:
-    #         line = ';"208";"72";"2018/OT3/922";"20180313";"3727";"21/03 (JR) HABLO CON CLIENTA, LE COMENTO QUE ES IMPOSIBLE TENERLO PARA HOY COMO ME DIJO ISRAEL, VAMOS A INTENTAR PARA EL VIERNES PERO ESTA COMLICADO, NO SE LO TOMA BIEN, HACE VARIOS DESPRECIOS, LE DIGO QUE ES UN LATERAL COMPLETO Y NO ES COSA DE DOS DIAS Y LA PIEZA SE HA RETRASADO EL PANEL DE PUERTA. DICE QUE HABLARA CON SU MADRE PARA QUE LLAME Y ""CANTE LAS 40"";'
-    #     else:
-    #         if not re.search(regex_dict['matches'], line):
-    #             line = re.sub(reg, '', line)
-    #
-    #         line = ''.join((';', line, ';'))
-    #
-    #         line = multireplace(line, replacements)
-    #
-    #     sys.stdout.write(line[1:-1])
+                current_delimiter_count = len(re.findall(reg_delimiter, line))
+                # print('current_delimiter_count', current_delimiter_count)
 
-    if method == 2:
-        # Method #2
-        with open(in_file_name) as in_file:
-            with open(out_file_name, 'w') as out_file_name:
+                if current_delimiter_count == delimiter_count:
+                    # print('all good!')
+                    prev_line = ''
+                    total_line = line
 
-                for line in in_file:
-                    if file_name == 'BIFD_SOHeaders_Comments.csv' and '"208";"72";"2018/OT3/922";"20180313";"3727";"21/03 (JR) HABLO CON CLIENTA' in line:
-                        print('Exception 1 found.')
-                        line = ';"208";"72";"2018/OT3/922";"20180313";"3727";"21/03 (JR) HABLO CON CLIENTA, LE COMENTO QUE ES IMPOSIBLE TENERLO PARA HOY COMO ME DIJO ISRAEL, VAMOS A INTENTAR PARA EL VIERNES PERO ESTA COMLICADO, NO SE LO TOMA BIEN, HACE VARIOS DESPRECIOS, LE DIGO QUE ES UN LATERAL COMPLETO Y NO ES COSA DE DOS DIAS Y LA PIEZA SE HA RETRASADO EL PANEL DE PUERTA. DICE QUE HABLARA CON SU MADRE PARA QUE LLAME Y ""CANTE LAS 40"";'
+                    total_line = ''.join((';', total_line, ';'))
 
-                    elif file_name == 'BIFD_SOHeaders_Comments.csv' and '"2";"24";"2019/AOJ/152";"20190704";"3024";"Agendado dia 12/08/2019 09:00 | Comp.ª Seguros""Logo"' in line:
-                        print('Exception 2 found.')
-                        line = ';Comp.ª Seguros""Logo"";'
+                    total_line, step_2_lines = multireplace(total_line, replacements, step_2_lines)
 
-                    elif file_name == 'BIFD_SOHeaders_Comments.csv' and '"2";"24";"2019/AOJ/159";"20190704";"3024";"Comp. Seg. ""Tranquilidade"' in line:
-                        print('Exception 3 found.')
-                        line = ';"2";"24";"2019/AOJ/159";"20190704";"3024";"Comp. Seg. ""Tranquilidade"";'
+                    out_file.write(total_line[1:-1])
+                    # print(total_line[1:-1])
 
-                    else:
-                        if not re.search(regex_dict['matches'], line):
-                            lines_step_1.append(line)
-                            line = re.sub(reg, '', line)
+                    # print('all good \n')
+                elif current_delimiter_count < delimiter_count:
+                    # print('inside')
+                    # print('prev_line', prev_line)
+                    total_line = ''.join((prev_line, line))
+                    # print('total line:', total_line)
 
-                        line = ''.join((';', line, ';'))
-
-                        line = multireplace(line, replacements)
-
-                    out_file_name.write(line[1:-1])
-
-    if method == 3:
-        # Method #3
-        current_delimiter_count, delimiter_count = 0, 0
-        prev_line, total_line, line = '', '', ''
-        with open(in_file_name) as in_file:
-            with open(out_file_name, 'w') as out_file_name:
-
-                for line in in_file:
-                    # print('prev_line: {} \nline: {}'.format(prev_line, line))
-                    if not count:
-                        delimiter_count = len(re.findall(reg_delimiter, line))
-                        # print('delimiter_count', delimiter_count)
-                    count = 1
-
-                    current_delimiter_count = len(re.findall(reg_delimiter, line))
+                    current_delimiter_count = len(re.findall(reg_delimiter, total_line))
                     # print('current_delimiter_count', current_delimiter_count)
 
-                    if current_delimiter_count == delimiter_count:
-                        # print('all good!')
-                        prev_line = ''
-                        total_line = line
+                    if current_delimiter_count < delimiter_count:
+                        # print('inside 2')
+                        total_line = re.sub(reg, '', total_line)
+                        step_1_count += 1
+                        # print('am i here? total line is {}'.format(total_line))
+                        prev_line = total_line
+                        # print(prev_line)
+                    else:
+                        # print('is line correct?')
+                        # print('total line is {} and current_delimiter_count is {}'.format(total_line, len(re.findall(reg_delimiter, total_line))))
+                        # total_line += '\r\n'
+                        # print('check here', total_line)
 
                         total_line = ''.join((';', total_line, ';'))
 
-                        total_line = multireplace(total_line, replacements)
+                        total_line, step_2_lines = multireplace(total_line, replacements, step_2_lines)
 
-                        out_file_name.write(total_line[1:-1])
+                        out_file.write(total_line[1:-1])
+                        prev_line = ''
                         # print(total_line[1:-1])
 
-                        # print('all good \n')
-                    elif current_delimiter_count < delimiter_count:
-                        # print('inside')
-                        # print('prev_line', prev_line)
-                        total_line = ''.join((prev_line, line))
-                        # print('total line:', total_line)
+        out_file.close()
+    in_file.close()
 
-                        current_delimiter_count = len(re.findall(reg_delimiter, total_line))
-                        # print('current_delimiter_count', current_delimiter_count)
-
-                        if current_delimiter_count < delimiter_count:
-                            # print('inside 2')
-                            total_line = re.sub(reg, '', total_line)
-                            # print('am i here? total line is {}'.format(total_line))
-                            prev_line = total_line
-                            # print(prev_line)
-                        else:
-                            # print('is line correct?')
-                            # print('total line is {} and current_delimiter_count is {}'.format(total_line, len(re.findall(reg_delimiter, total_line))))
-                            # total_line += '\r\n'
-                            # print('check here', total_line)
-
-                            total_line = ''.join((';', total_line, ';'))
-
-                            total_line = multireplace(total_line, replacements)
-
-                            out_file_name.write(total_line[1:-1])
-                            prev_line = ''
-                            # print(total_line[1:-1])
+    time_to_normalize = time.time() - start
+    print('File {} normalized. Elapsed Time: {:.2f}. \n'.format(file_name, time_to_normalize))
+    return step_1_count, step_2_lines, time_to_normalize
 
 
 if __name__ == '__main__':
